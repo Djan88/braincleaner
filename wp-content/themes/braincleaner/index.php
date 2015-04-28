d3f2a14d64057e1097eae208c478e48a56a654c1
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-04-28 15:28:53 +0300

    theme fix-2

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -52,26 +52,38 @@
               <h3 class="panel-title">Вход на сайт</h3>
             </div>
             <div class="panel-body">
-              <form class="form-horizontal user-in" role="form">
+              <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post" class="form-horizontal user-in" role="form">
                 <div class="form-group">
                   <label for="inputEmail3" class="col-sm-2 control-label">Логин</label>
                   <div class="input-group input-group-lg">
                     <span class="input-group-addon">@</span>
-                    <input type="text" class="form-control" placeholder="Логин">
+                    <label for="user_login"><?php _e('Username') ?><br />
+                      <input type="text" name="log" id="user_login" class="input" value="<?php echo esc_attr($user_login); ?>" size="20" />
+                    </label>
                   </div>
                 </div>
+                <?php
+                /**
+                 * Fires following the 'Password' field in the login form.
+                 *
+                 * @since 2.1.0
+                 */
+                do_action( 'login_form' );
+                ?>
                 <div class="form-group">
                   <label for="inputPassword3" class="col-sm-2 control-label">Пароль</label>
                   <div class="input-group input-group-lg">
                     <span class="input-group-addon">*</span>
-                    <input type="text" class="form-control" placeholder="Пароль">
+                    <label for="user_pass"><?php _e('Password') ?><br />
+                      <input type="password" name="pwd" id="user_pass" class="input" value="" size="20" />
+                    </label>
                   </div>
                 </div>
                 <div class="form-group">
                   <div class="col-sm-offset-2 col-sm-10">
                     <div class="checkbox">
                       <label>
-                        <input type="checkbox"> Запомнить меня
+                        <label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label>
                       </label>
                     </div>
                   </div>
@@ -79,7 +91,17 @@
                 <div class="form-group">
                   <div class="col-sm-offset-2 col-sm-10">
                     <!-- <button type="submit" class="btn btn-default">Войти</button> -->
-                    <a href="inside.html" class="btn btn-default">Войти</a>
+                    <!-- <a href="inside.html" class="btn btn-default">Войти</a> -->
+                            <input type="submit" name="wp-submit" id="wp-submit" class="btn btn-default" value="<?php esc_attr_e('Log In'); ?>" />
+                    <?php   if ( $interim_login ) { ?>
+                            <input type="hidden" name="interim-login" value="1" />
+                    <?php   } else { ?>
+                            <input type="hidden" name="redirect_to" value="<?php echo esc_attr($redirect_to); ?>" />
+                    <?php   } ?>
+                    <?php   if ( $customize_login ) : ?>
+                            <input type="hidden" name="customize-login" value="1" />
+                    <?php   endif; ?>
+                            <input type="hidden" name="testcookie" value="1" />
                   </div>
                 </div>
               </form>
