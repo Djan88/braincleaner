29cf5888e2af5c01c14491a4128fed770554302c
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-04-28 16:25:34 +0300

    cropped-11

---
+++ wp-content/themes/functions.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Author: Eddie Machado
+URL: htp://themble.com/bones/
+
+This is where you can drop your custom functions or
+just edit things like thumbnail sizes, header images,
+sidebars, comments, ect.
+*/
+
+function users_redirect(){
+wp_redirect(site_url('/'));
+die();
+}
+if(!current_user_can('manage_options')){
+add_action('admin_init','users_redirect');
+add_filter('login_redirect', 'users_redirect');
+}
+
+/* DON'T DELETE THIS CLOSING TAG */ ?>
