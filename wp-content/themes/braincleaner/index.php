0192cb0ddc5a7e1f66010800c5383d383e74e331
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-04-29 12:26:20 +0300

    cropped-25

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -15,13 +15,6 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <?php wp_nav_menu(array('menu' => 'Меню в шапке', 'container' => false, menu_class => 'nav navbar-nav' )); ?>
-              <!-- <ul class="nav navbar-nav">
-                <li class="active"><a href="index.html"><span class="glyphicon glyphicon-film"></span> Видео материалы</a></li>
-                <li><a href="index.html"><span class="glyphicon glyphicon-book"></span> Статьи и книги</a></li>
-                <li><a href="index.html"><span class="glyphicon glyphicon-pencil"></span> Запись на прием</a></li>
-                <li><a href="index.html"><span class="glyphicon glyphicon-envelope"></span> Написать письмо</a></li>
-                <li><a href="/wp-login.php?action=logout&amp;_wpnonce=a6cad512ba"><span class="glyphicon glyphicon-log-out"></span> Выход</a></li>
-              </ul> -->
               <div class="pull-right" style="width: 31.5%;margin-top: 7px;">
                 <div class="input-group">
                   <input type="text" class="form-control">
@@ -192,21 +185,15 @@
             <a class="navbar-brand" href="#">Меню</a>
           </div>
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="index.html"><span class="glyphicon glyphicon-film"></span> Видео материалы</a></li>
-              <li><a href="index.html"><span class="glyphicon glyphicon-book"></span> Статьи и книги</a></li>
-              <!-- <li><a href="index.html"><span class="glyphicon glyphicon-pencil"></span> Запись на прием</a></li> -->
-              <li><a href="index.html"><span class="glyphicon glyphicon-envelope"></span> Написать письмо</a></li>
-              <li><a href="index.html"><span class="glyphicon glyphicon-log-out"></span> Выход</a></li>
-              <li class="pull-right" style="width: 31.5%;margin-top: 7px;">
-                <div class="input-group">
-                  <input type="text" class="form-control">
-                  <span class="input-group-btn">
-                    <button class="btn btn-default search" type="button">Поиск</button>
-                  </span>
-                </div><!-- /input-group -->
-              </li>
-            </ul>
+            <?php wp_nav_menu(array('menu' => 'Меню в шапке', 'container' => false, menu_class => 'nav navbar-nav' )); ?>
+            <div class="pull-right" style="width: 31.5%;margin-top: 7px;">
+              <div class="input-group">
+                <input type="text" class="form-control">
+                <span class="input-group-btn">
+                  <button class="btn btn-default search" type="button">Поиск</button>
+                </span>
+              </div><!-- /input-group -->
+            </div>
           </div>
         </div>
       </div>
