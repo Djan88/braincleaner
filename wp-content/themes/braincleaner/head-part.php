570bbaca1b67302bc1e8602058a797db124576bc
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-04-29 17:30:18 +0300

    status-20

--- wp-content/themes/braincleaner/head-part.php
+++ wp-content/themes/braincleaner/head-part.php
@@ -12,12 +12,14 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <?php wp_nav_menu(array('menu' => 'Меню в шапке', 'container' => false, menu_class => 'nav navbar-nav' )); ?>
       <div class="pull-right" style="width: 31.5%;margin-top: 7px;">
-        <div class="input-group">
+          <form method="get" name="searchform" id="searchform" action="<?php bloginfo('siteurl')?>">
+            <div class="input-group">
               <input type="text" name="s" id="s" placeholder="Что искать?" class="search-input form-control" required="true"/>
               <span class="input-group-btn">
                 <input id="btnSearch" type="submit" name="submit" value="<?php _e('Поиск'); ?>" class="search-button btn btn-default search"/>
               </span>
-        </div><!-- /input-group -->
+            </div><!-- /input-group -->
+          </form>
       </div>
     </div>
   </div>
