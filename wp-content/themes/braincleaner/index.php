cb4395091395bb424413c1e589a8a7f58f6e820e
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-04-29 17:18:43 +0300

    status-14

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -10,33 +10,27 @@
           <?php if(is_archive()) { ?>
             <?php include(TEMPLATEPATH . '/arch.php'); ?>
           <?php } else { ?>
-            <h3>Видео материалы</h3>
-            <div class="content-wrap">
-              <div class="panel panel-default main_heading">
-                <h4 class="panel-heading"><span class="glyphicon glyphicon-film"></span> <a href="/">Биологическое центрирование, базовые уровни в клинике Чикурова</a> <span class="pull-right date-block">01.04.2015</span></h4>
-                <div class="panel-body">
-                  <p>Небольшой блок для описания видео записи. Можно не заполнять</p>
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/aKs8opb3U1A" frameborder="0" allowfullscreen></iframe>
-                  <a href="/" class="button"></a>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+              <?php if(is_single() || is_page()) { ?>
+                <h3><?php the_title(); ?></h3>
+              <?php } else { ?>
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+              <?php } ?>
+              <div class="content-wrap">
+                <div class="panel panel-default main_heading">
+                  <div class="panel-body">
+                    <?php
+                    the_content(__('(more...)'));
+                    wp_link_pages();
+                    edit_post_link(__('Edit This'));
+                    ?>
+                  </div>
                 </div>
               </div>
-              <div class="panel panel-default main_heading">
-                <h4 class="panel-heading"><span class="glyphicon glyphicon-film"></span> <a href="/">Закрытыий доклад в клинике по материалам экспедиции в Центральную и Южную Америку</a> <span class="pull-right date-block">21.06.2014</span></h4>
-                <div class="panel-body">
-                  <p>Небольшой блок для описания видео записи. Можно не заполнять</p>
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/09sxmilzzBc" frameborder="0" allowfullscreen></iframe>
-                  <a href="/" class="button"></a>
-                </div>
-              </div>
-              <div class="panel panel-default main_heading">
-                <h4 class="panel-heading"><span class="glyphicon glyphicon-film"></span> <a href="/">Откуда берутся мысли и для чего нужны эмоции</a> <span class="pull-right date-block">02.05.2014</span></h4>
-                <div class="panel-body">
-                  <p>Небольшой блок для описания видео записи. Можно не заполнять</p>
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/LGO_CSrSuNg" frameborder="0" allowfullscreen></iframe>
-                  <a href="/" class="button"></a>
-                </div>
-              </div>
-            </div>
+            <?php endwhile; else: ?>
+              <?php _e('Sorry, no posts matched your criteria.'); ?>
+            <?php endif; ?>
+            <?php posts_nav_link(' &#8212; ', __('&laquo; Более новые '), __('Более старые &raquo;')); ?>
           <?php } ?>
           </div>
           <?php include(TEMPLATEPATH . '/sidebar.php'); ?>
