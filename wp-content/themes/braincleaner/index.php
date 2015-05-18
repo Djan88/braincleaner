aa6ae60c7e7578c68574028c5796348e1e2d1627
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-18 12:10:42 +0300

    defra-16

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -58,12 +58,14 @@
                           edit_post_link(__('Edit This'));
                           ?>
                         </p>
-                        <a href="/category/defragmentaciya/" class="btn btn-default pull-left"><span class="glyphicon glyphicon-arrow-left"></span> Назад</a>
-                        <div class="btn-group">
-                          <button type="button" class="btn btn-default btn-status btn-auto">Автоматический режим</button>
-                          <button type="button" class="btn btn-default btn-status btn-manual">Ручное управление</button>
-                        </div>
-                        <button type="button" class="btn btn-default pull-right"><span class="glyphicon glyphicon-leaf"></span> Старт процедуры</button>
+                    </div>
+                    <div class="col-md-12">
+                      <a href="/category/defragmentaciya/" class="btn btn-default pull-left"><span class="glyphicon glyphicon-arrow-left"></span> Назад</a>
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-status btn-auto">Автоматический режим</button>
+                        <button type="button" class="btn btn-default btn-status btn-manual">Ручное управление</button>
+                      </div>
+                      <button type="button" class="btn btn-default pull-right"><span class="glyphicon glyphicon-leaf"></span> Старт процедуры</button>
                     </div>
                   <?php } else { ?>
                     <?php
