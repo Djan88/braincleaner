a7be124dfacfa41e69c74bfc4683644a2abf7ea1
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-18 13:39:38 +0300

    defra-28

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -65,7 +65,7 @@
                           <a href="/category/defragmentaciya/" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span> Назад</a>
                         </div>
                         <div class="btn-group">
-                        <button type="button" class="btn btn-default btn-status btn-auto">Авто режим</button>
+                        <button type="button" class="btn btn-default btn-status btn-auto active">Авто режим</button>
                         </div>
                         <div class="btn-group">
                         <button type="button" class="btn btn-default btn-status btn-manual">Ручное управление</button>
