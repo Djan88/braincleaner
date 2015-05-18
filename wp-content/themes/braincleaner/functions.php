4a989c01b847b1b9ea31c95a6ac6eb63e3a1c435
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-18 14:48:39 +0300

    defra-45

--- wp-content/themes/braincleaner/functions.php
+++ wp-content/themes/braincleaner/functions.php
@@ -37,7 +37,7 @@ add_filter('login_redirect', 'users_redirect');
         'after_title' => '</a></h4>',
     ));
 
-if (!current_user_can('administrator')){
-  add_filter('show_admin_bar', '__return_false');
-}
+    if (!current_user_can('administrator')):
+      show_admin_bar(false);
+    endif;
 /* DON'T DELETE THIS CLOSING TAG */ ?>
