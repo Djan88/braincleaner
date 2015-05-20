905c6bb03197cab890479c76ecf9253278ebf471
Ivan Avraamov <avraamov@tadatuta.com>
2015-05-20 08:56:14 +0300

    procedure

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -44,10 +44,10 @@
                     </div>
                   <?php } else if(in_category(5)) { ?>
                       <div class="b-popup hidden">
-                        <div class="b-popup-content clearfix" style="width: 80%;">
+                        <div class="b-popup-content clearfix">
                             <?php if(get_field('images')): ?>
                               <?php while(has_sub_field('images')): ?>
-                                  <div class="col-md-3">
+                                  <div class="col-md-12">
                                     <img src="<?php the_sub_field('image') ?>" alt=""> 
                                   </div>
                               <?php endwhile; ?>
