543890e7c881f51160218e85108bc6949876b78f
Ivan Avraamov <avraamov@tadatuta.com>
2015-05-20 18:48:05 +0300

    protocol-22

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -54,13 +54,13 @@
                         </div>
                         <div class="b-popup-content clearfix">
                           <div class="row">
-                            <?php $defNum = 0; ?>
+                            <?php $defNum = 0 ?>
                             <?php if(get_field('images')): ?>
                               <?php while(has_sub_field('images')): ?>
-                                  <div class="col-md-6 col-md-offset-3 popup-img-wrap hidden" data-defNum="<?php $defNum; ?>">
+                                  <div class="col-md-6 col-md-offset-3 popup-img-wrap hidden" data-defNum="<?php $defNum ?>">
                                     <img src="<?php the_sub_field('image') ?>" alt=""> 
                                   </div>
-                                  <?php $defNum += 1; ?>
+                                  <?php $defNum += 1 ?>
                               <?php endwhile; ?>
                             <?php endif; ?>
                           </div>
