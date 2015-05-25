a08c136d3176d31d5c117f7a142654939a306942
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-25 15:45:46 +0300

    prot fix-57

--- wp-content/themes/braincleaner/js/scripts.js
+++ wp-content/themes/braincleaner/js/scripts.js
@@ -114,8 +114,9 @@ $(document).ready(function() {
                 jQuery('.popup-img-wrap').addClass('hidden');
                 jQuery('.popup-img-wrap[data-defNum='+defCount+']').removeClass('hidden');
                 defCount += 1;
-                if(jQuery('.popup-img-wrap[data-defNum='+defCount+']').data('formula')){
+                if(jQuery('.popup-img-wrap[data-defNum='+defCount-1+']').data('formula')&&jQuery('.popup-img-wrap[data-defNum='+defCount-1+']').data('checked') !== '1'){
                     defCount -= 1;
+                    jQuery('.popup-img-wrap[data-defNum='+defCount-1+']').data('checked', '1')
                 }
                 console.log('test');
                 jQuery('.protocol_stop, .protocol_close').on('click', function() {
