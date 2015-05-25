54aaec3d5075e2365f41fd09b07e5172dff13cdf
Ivan Avraamov <avraamov@tadatuta.com>
2015-05-25 22:02:13 +0300

    protocol-38

--- wp-content/themes/braincleaner/head-part.php
+++ wp-content/themes/braincleaner/head-part.php
@@ -10,8 +10,12 @@
       <a class="navbar-brand" href="#">Меню</a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <?php wp_nav_menu(array('menu' => 'Меню в шапке', 'container' => false, menu_class => 'nav navbar-nav' )); ?>
-      <div class="pull-right" style="width: 31.5%;margin-top: 7px;">
+      <div class="row">
+        <div class="col-md-9">
+          <?php wp_nav_menu(array('menu' => 'Меню в шапке', 'container' => false, menu_class => 'nav navbar-nav' )); ?>
+        </div>
+      </div>
+      <div class="col-md-3">
           <form method="get" name="searchform" id="searchform" action="<?php bloginfo('siteurl')?>">
             <div class="input-group">
               <input type="text" name="s" id="s" placeholder="Что искать?" class="search-input form-control" required="true"/>
