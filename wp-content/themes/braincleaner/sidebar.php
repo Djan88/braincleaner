3883dc384b622438a96d10ba678402ec0fae1ca0
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-28 13:48:11 +0300

    rights fix-10

--- wp-content/themes/braincleaner/sidebar.php
+++ wp-content/themes/braincleaner/sidebar.php
@@ -1,7 +1,7 @@
 <div class="col-md-4 sidebar-zone">
   <div class="list-group">
     <div class="list-group-item active">
-      <h4 class="list-group-item-heading">Важная информация</h4>
+      <h4 class="list-group-item-heading">Новости</h4>
     </div>
     <?php if (!dynamic_sidebar("arhive-widget-area") ) : ?>
     <?php endif; ?>
