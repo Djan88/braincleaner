6c2624deae96cfa581a6b0467ed7b2edb4631623
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-28 11:52:36 +0300

    rights fix

--- wp-content/themes/braincleaner/arch.php
+++ wp-content/themes/braincleaner/arch.php
@@ -6,19 +6,13 @@
 </div>
 <div class="content-wrap clearfix">
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div class="panel panel-default main_heading">
-      <h4 class="panel-heading">
-        <?php if(is_category(2)) { ?>
-          <span class="glyphicon glyphicon-film"></span>
-        <?php } else if(is_category(3)) { ?>
+    <?php if(is_category(3)){ ?>
+      <div class="panel panel-default main_heading">
+        <h4 class="panel-heading">
           <span class="glyphicon glyphicon-book"></span>
-        <?php } else if(is_category(5)) { ?>
-          <span class="glyphicon glyphicon-leaf"></span>
-        <?php } ?>
-        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <?php if(!is_category(5)) { ?><span class="pull-right date-block"><?php the_time('j M Y'); ?></span><?php } ?>
-      </h4>
-      <div class="panel-body">
-        <?php if(is_category(3)||is_category(5)) { ?>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><span class="pull-right date-block"><?php the_time('j M Y'); ?></span>
+        </h4>
+        <div class="panel-body">
           <?php if( get_the_post_thumbnail()){ ?>
             <div class="col-md-6 thumb-wrap">
               <?php the_post_thumbnail( 'medium' ); ?>
@@ -40,28 +34,66 @@
             <?php if( get_field('ppt')){ ?>
               <a href="<?php the_field('ppt')?>" class="down down_ppt" data-toggle="tooltip" data-placement="right" title="Скачать в формате Microsoft PowerPoint"></a>
             <?php } ?>
-            <?php if(is_category(5)) { ?>
-              <div class="defra_content">
+          </div>
+        </div>
+      </div>
+    <?php } else if(is_category(5)){ ?>
+      <div class="panel panel-default main_heading">
+        <h4 class="panel-heading">
+          <span class="glyphicon glyphicon-leaf"></span>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h4>
+        <div class="panel-body">
+          <?php if( get_the_post_thumbnail()){ ?>
+            <div class="col-md-6 thumb-wrap">
+              <?php the_post_thumbnail( 'medium' ); ?>
+            </div>
+            <div class="col-md-6">
+          <?php } else { ?>
+            <div>
+          <?php } ?>
+            <?php if( get_field('knt')){ ?>
+              <p>
                 <?php
                 the_content(__('(more...)'));
                 wp_link_pages();
                 edit_post_link(__('Edit This'));
                 ?>
-              </div>
-              <div class="defra_btns">
-                <a href="<?php the_permalink(); ?>" class="btn btn-default btn-lg btn-block center-block">Выбор режима</a>
-              </div>
+              </p>
+              <a href="<?php the_field('knt')?>" class="down down_knt" data-toggle="tooltip" data-placement="right" title="Скачать в формате Apple Keynote"></a>
             <?php } ?>
+            <?php if( get_field('ppt')){ ?>
+              <a href="<?php the_field('ppt')?>" class="down down_ppt" data-toggle="tooltip" data-placement="right" title="Скачать в формате Microsoft PowerPoint"></a>
+            <?php } ?>
+            <div class="defra_content">
+              <?php
+              the_content(__('(more...)'));
+              wp_link_pages();
+              edit_post_link(__('Edit This'));
+              ?>
+            </div>
+            <div class="defra_btns">
+              <a href="<?php the_permalink(); ?>" class="btn btn-default btn-lg btn-block center-block">Выбор режима</a>
+            </div>
           </div>
-        <?php } else { ?>
-          <?php
-          the_content(__('(more...)'));
-          wp_link_pages();
-          edit_post_link(__('Edit This'));
-          ?>
-        <?php } ?>
+        </div>
       </div>
-    </div>
+    <?php } else { ?>
+      <div class="panel panel-default main_heading">
+        <h4 class="panel-heading">
+            <span class="glyphicon glyphicon-film"></span>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><span class="pull-right date-block"><?php the_time('j M Y'); ?></span>
+        </h4>
+        <div class="panel-body">
+            <?php
+            the_content(__('(more...)'));
+            wp_link_pages();
+            edit_post_link(__('Edit This'));
+            ?>
+          <?php } ?>
+        </div>
+      </div>
+    <?php } ?>
   <?php endwhile; else: ?>
     <?php _e('Sorry, no posts matched your criteria.'); ?>
   <?php endif; ?>
