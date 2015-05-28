5e3d3880826071a24afc4d1db5d74da295f2c666
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-28 12:25:22 +0300

    rights fix-2

--- wp-content/themes/braincleaner/arch.php
+++ wp-content/themes/braincleaner/arch.php
@@ -7,36 +7,42 @@
 <div class="content-wrap clearfix">
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <?php if(is_category(3)){ ?>
-      <div class="panel panel-default main_heading">
-        <h4 class="panel-heading">
-          <span class="glyphicon glyphicon-book"></span>
-          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><span class="pull-right date-block"><?php the_time('j M Y'); ?></span>
-        </h4>
-        <div class="panel-body">
-          <?php if( get_the_post_thumbnail()){ ?>
-            <div class="col-md-6 thumb-wrap">
-              <?php the_post_thumbnail( 'medium' ); ?>
+      <?php if ( is_user_logged_in() ) { ?>
+        <div class="panel panel-default main_heading">
+          <h4 class="panel-heading">
+            <span class="glyphicon glyphicon-book"></span>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><span class="pull-right date-block"><?php the_time('j M Y'); ?></span>
+          </h4>
+          <div class="panel-body">
+            <?php if( get_the_post_thumbnail()){ ?>
+              <div class="col-md-6 thumb-wrap">
+                <?php the_post_thumbnail( 'medium' ); ?>
+              </div>
+              <div class="col-md-6">
+            <?php } else { ?>
+              <div>
+            <?php } ?>
+              <?php if( get_field('knt')){ ?>
+                <p>
+                  <?php
+                  the_content(__('(more...)'));
+                  wp_link_pages();
+                  edit_post_link(__('Edit This'));
+                  ?>
+                </p>
+                <a href="<?php the_field('knt')?>" class="down down_knt" data-toggle="tooltip" data-placement="right" title="Скачать в формате Apple Keynote"></a>
+              <?php } ?>
+              <?php if( get_field('ppt')){ ?>
+                <a href="<?php the_field('ppt')?>" class="down down_ppt" data-toggle="tooltip" data-placement="right" title="Скачать в формате Microsoft PowerPoint"></a>
+              <?php } ?>
             </div>
-            <div class="col-md-6">
-          <?php } else { ?>
-            <div>
-          <?php } ?>
-            <?php if( get_field('knt')){ ?>
-              <p>
-                <?php
-                the_content(__('(more...)'));
-                wp_link_pages();
-                edit_post_link(__('Edit This'));
-                ?>
-              </p>
-              <a href="<?php the_field('knt')?>" class="down down_knt" data-toggle="tooltip" data-placement="right" title="Скачать в формате Apple Keynote"></a>
-            <?php } ?>
-            <?php if( get_field('ppt')){ ?>
-              <a href="<?php the_field('ppt')?>" class="down down_ppt" data-toggle="tooltip" data-placement="right" title="Скачать в формате Microsoft PowerPoint"></a>
-            <?php } ?>
           </div>
         </div>
-      </div>
+      <?php } else { ?>
+        <div class="not_logged">
+          Контент доступен только зарегистрированным пользователям!
+        </div>
+      <?php } ?>
     <?php } else if(is_category(5)){ ?>
       <div class="panel panel-default main_heading">
         <h4 class="panel-heading">
