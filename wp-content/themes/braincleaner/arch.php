a452848fbef8c73032f2622f353f695ade3e9d03
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-28 12:35:28 +0300

    rights fix-8

--- wp-content/themes/braincleaner/arch.php
+++ wp-content/themes/braincleaner/arch.php
@@ -94,24 +94,29 @@
         <?php endif; ?>
         <?php posts_nav_link(' &#8212; ', __('&laquo; Более новые '), __('Более старые &raquo;')); ?>
       <?php } else { ?>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <div class="panel panel-default main_heading">
-            <h4 class="panel-heading">
-                <span class="glyphicon glyphicon-film"></span>
-              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><span class="pull-right date-block"><?php the_time('j M Y'); ?></span>
-            </h4>
-            <div class="panel-body">
-                <?php
-                the_content(__('(more...)'));
-                wp_link_pages();
-                edit_post_link(__('Edit This'));
-                ?>
-            </div>
+        <div class="not_logged">
+          Информация в этом разделе доступна только зарегистрированным пользователям!
+        </div>
+      <?php } ?>
+    <?php } else { ?>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <div class="panel panel-default main_heading">
+          <h4 class="panel-heading">
+              <span class="glyphicon glyphicon-film"></span>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><span class="pull-right date-block"><?php the_time('j M Y'); ?></span>
+          </h4>
+          <div class="panel-body">
+              <?php
+              the_content(__('(more...)'));
+              wp_link_pages();
+              edit_post_link(__('Edit This'));
+              ?>
           </div>
-        <?php endwhile; else: ?>
-          <?php _e('Sorry, no posts matched your criteria.'); ?>
-        <?php endif; ?>
-        <?php posts_nav_link(' &#8212; ', __('&laquo; Более новые '), __('Более старые &raquo;')); ?>
+        </div>
+      <?php endwhile; else: ?>
+        <?php _e('Sorry, no posts matched your criteria.'); ?>
+      <?php endif; ?>
+      <?php posts_nav_link(' &#8212; ', __('&laquo; Более новые '), __('Более старые &raquo;')); ?>
       <?php } else { ?>
         <div class="not_logged">
           Информация в этом разделе доступна только зарегистрированным пользователям!
