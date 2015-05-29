f4c11c93461e216918c645405595156a370da1f6
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-05-29 13:06:36 +0300

    rights fix-25

--- wp-content/themes/braincleaner/js/scripts.js
+++ wp-content/themes/braincleaner/js/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    jQuery('.panel-body').find('iframe').css('height', '400px');
+    var wideoHeight = function(){
+        var video_w = parseFloat(jQuery('.video').find('iframe').css('width'));
+        jQuery('.panel-body').find('iframe').css('height', video_w/1.5+'px');
+    }
+    wideoHeight();
+    jQuery(window).resize(function() {
+      console.log(parseFloat(jQuery('.panel-body').find('iframe').css('width')));
+      wideoHeight();
+    });
 
     var curStatus = 'auto',
         phase,
