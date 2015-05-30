7d895a130147226cfabbb09ac791019c9000cd85
Ivan Avraamov <avraamov@tadatuta.com>
2015-05-30 20:57:01 +0300

    protocol-55

--- wp-content/themes/braincleaner/footer.php
+++ wp-content/themes/braincleaner/footer.php
@@ -2,5 +2,31 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <?php wp_footer(); ?>
     <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter30639627 = new Ya.Metrika({id:30639627,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true});
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="//mc.yandex.ru/watch/30639627" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
   </body>
 </html>
