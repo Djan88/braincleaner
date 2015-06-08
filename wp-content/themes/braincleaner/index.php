e55ccf2a5742b8e047ab9310390ec7d9c80b8cde
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-06-08 13:13:12 +0300

    img sidebar fix-5

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -237,8 +237,9 @@
       <div class="row footer-wrap">
         <div class="container">
           <div class="pull-left">
-            <a href="/">Braincleaner</a> 2015 <a href="http://braincleaner.ru/wp-login.php?action=logout&_wpnonce=bc0c947e8d"><span class="glyphicon glyphicon-log-out"></span> Выход</a>
+            <a href="/">Braincleaner</a> 2015
           </div>
+          <a style="" href="http://braincleaner.ru/wp-login.php?action=logout&_wpnonce=bc0c947e8d"><span class="glyphicon glyphicon-log-out"></span> Выход</a>
           <div class="pull-right">
             <a href="mailto:info@defra.ru">info@braincleaner.ru</a>
           </div>
