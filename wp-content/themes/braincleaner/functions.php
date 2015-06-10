4a2f70a25bf47837a1b1f4f684d766039216f80e
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-06-10 13:40:39 +0300

    users-4

--- wp-content/themes/braincleaner/functions.php
+++ wp-content/themes/braincleaner/functions.php
@@ -39,6 +39,15 @@ add_filter('login_redirect', 'users_redirect');
 
 
 // Функционал добавления пользователей
+    // let's start by enqueuing our styles correctly
+    function wptutsplus_admin_styles() {
+        
+        wp_enqueue_style('jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
+        wp_register_style( 'admin_style', gavern_file_uri('css/back-end/admin-style.css') );
+        wp_enqueue_style( 'admin_style' );
+        wp_enqueue_script('admin-scripts', gavern_file_uri('js/admin-scripts.js'), array('jquery'));
+        wp_enqueue_script('jquery-ui-datepicker'); 
+    }
     add_action( 'admin_enqueue_scripts', 'wptutsplus_admin_styles' );
 
     add_action( 'admin_menu', 'add_reestr_masters' );
