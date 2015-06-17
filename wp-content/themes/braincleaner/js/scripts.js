59bda6b79182ad1bdfb23e682f330b72432053a0
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-06-17 13:40:08 +0300

    sound fix-11

--- wp-content/themes/braincleaner/js/scripts.js
+++ wp-content/themes/braincleaner/js/scripts.js
@@ -3,22 +3,29 @@
 //     preload: true
 // });
 
+var isUnlocked = false;
+function unlock() {
+            
+    if(isIOS || this.unlocked)
+        return;
+
+    // create empty buffer and play it
+    var buffer = myContext.createBuffer(1, 1, 22050);
+    var source = myContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(myContext.destination);
+    source.noteOn(0);
+
+    // by checking the play state after some time, we know if we're really unlocked
+    setTimeout(function() {
+        if((source.playbackState === source.PLAYING_STATE || source.playbackState === source.FINISHED_STATE)) {
+            isUnlocked = true;
+        }
+    }, 0);
+
+}
+
 jQuery(document).ready(function() {
-    window.addEventListener('touchstart', function() {
-
-        // create empty buffer
-        var buffer = myContext.createBuffer(1, 1, 22050);
-        var source = myContext.createBufferSource();
-        source.buffer = buffer;
-
-        // connect to output (your speakers)
-        source.connect(myContext.destination);
-
-        // play the file
-        source.noteOn(0);
-
-    }, false);
-
     ion.sound({
         sounds: [
             {
@@ -29,7 +36,7 @@ jQuery(document).ready(function() {
         path: "/sounds/",
         preload: true 
     });
-    
+
     jQuery('.panel-body').find('iframe').css('height', '400px');
     var wideoHeight = function(){
         var video_w = parseFloat(jQuery('.panel-body').find('iframe').css('width'));
