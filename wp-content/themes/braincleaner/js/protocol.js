c246cbc3429f630e65c45e574f5517582fbb699d
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-10-09 15:22:20 +0300

    main fix-62

--- wp-content/themes/braincleaner/js/protocol.js
+++ wp-content/themes/braincleaner/js/protocol.js
@@ -25,6 +25,7 @@ circle_model = function(count_animation){
     if(count_animation <= 120){
         cur_animation_val += 1.5;
         d12Val+= 9;
+        jQuery('.ring-formula').css('transform', 'rotate(-'+d12Val+'deg)');
         jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
         jQuery('.ring-formula').css('background', '#fff url(/wp-content/themes/braincleaner/img/lovushka.jpg) 0 0/100% no-repeat');
     } else if (count_animation >= 120 && count_animation <= 228){
