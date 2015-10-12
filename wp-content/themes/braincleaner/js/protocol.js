075d11d6eb0591b1b3d3df4fad98247b678d7740
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-10-12 16:06:40 +0300

    main fix-109

--- wp-content/themes/braincleaner/js/protocol.js
+++ wp-content/themes/braincleaner/js/protocol.js
@@ -73,7 +73,8 @@ jQuery(document).ready(function() {
         prot_count = 1;
         cur_animation_val = 0;
         count_animation = 1;
-        jQuery('.sq3').css('background', 'url('+faces_img+') 55px/0 11000px no-repeat');
+        jQuery('.sq3').css('background', 'url('+faces_img+') no-repeat');
+        jQuery('.sq3').css('background-size', '11000px');
         phaseOne = setInterval(function(){
             if(count_animation <= 360){
                 circle_model(count_animation);
