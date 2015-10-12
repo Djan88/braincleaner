b91ad535e0eff68c88e8adff844e1e868c1111f5
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-10-12 13:06:16 +0300

    main fix-86

--- wp-content/themes/braincleaner/js/scripts.js
+++ wp-content/themes/braincleaner/js/scripts.js
@@ -58,7 +58,7 @@ jQuery(document).ready(function() {
         cur_recep = jQuery(this).data('usex');
         next_screen(2);
         jQuery('.sex_item-recep').removeClass('active');
-        jQuery('#menu-item-646').css('display', 'inline_block');
+        jQuery('#menu-item-646').css('display', 'inlineBlock');
         jQuery(this).addClass('active');
         protocol_people(cur_sex, cur_recep);
     });
