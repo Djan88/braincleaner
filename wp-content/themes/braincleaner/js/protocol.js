c8059fdbe8bda260ebc06f46cf7c75adbcff4615
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-10-13 17:31:11 +0300

    main fix-124

--- wp-content/themes/braincleaner/js/protocol.js
+++ wp-content/themes/braincleaner/js/protocol.js
@@ -94,6 +94,36 @@ jQuery(document).ready(function() {
             }
         }, 250);
     }
+    mw = function(){
+        jQuery('.sq1').css('background', 'url('+client_img+') no-repeat');
+        jQuery('.sq1').addClass('client_sq');
+    //фаза 1
+        reloadTime = 0;
+        reloadTime1 = 0;
+        d12Val = 0;
+        prot_count = 1;
+        cur_animation_val = 0;
+        count_animation = 1;
+        jQuery('.sq3').css('background', 'url('+faces_img+') no-repeat');
+        jQuery('.sq3').css('background-size', '11000px');
+        jQuery('.sq3').css('background-position-x', '-55px');
+        phaseOne = setInterval(function(){
+            if(count_animation <= 360){
+                circle_model(count_animation);
+                count_animation += 1;
+            } else {
+                if (prot_count <= 31) {
+                    prot_count += 1;
+                    cur_faces = parseInt(jQuery('.sq3').css('background-position-x'))-357;
+                    jQuery('.sq3').css('background-position-x', cur_faces+'px');
+                    count_animation = 1;
+                    d12Val = 0;
+                } else {
+                    clearInterval(phaseOne);
+                }
+            }
+        }, 250);
+    }
     jQuery('body').on('click', '.prot-start', function(event) {
         console.log(circle_protocol);
         if (circle_protocol && circle_protocol == 'mw') {
