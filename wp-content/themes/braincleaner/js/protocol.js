4d9897bd17ebebff20cfe4a0de396bb9b3556b0c
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-10-14 14:50:28 +0300

    main fix-153

--- wp-content/themes/braincleaner/js/protocol.js
+++ wp-content/themes/braincleaner/js/protocol.js
@@ -204,6 +204,51 @@ jQuery(document).ready(function() {
             }
         }, 250);
     };
+    ww = function(){
+        client_img = jQuery('body').find('.injected').attr('src');
+        jQuery('.sq3').css('background', 'url('+client_img+') no-repeat');
+        jQuery('.sq3').addClass('client_sq');
+    //фаза 1
+        reloadTime = 0;
+        reloadTime1 = 0;
+        d12Val = 0;
+        prot_count = 1;
+        cur_animation_val = 0;
+        count_animation = 1;
+        tickSound.play();
+        jQuery('.sq2').css('background', 'url('+faces_img+') no-repeat');
+        jQuery('.sq2').css('background-position-x', '-95px');
+        phaseOne = setInterval(function(){
+            if(count_animation <= 360){
+                circle_model(count_animation);
+                count_animation += 1;
+            } else {
+                if (prot_count <= 15) {
+                    tickSound.stop();
+                    reloadSound.play();
+                    if (caliber == 1){
+                        caliber = 2;
+                        jQuery('.sq4').css('background', 'url('+faces_img+') no-repeat');
+                        jQuery('.sq4').css('background-position-x', jQuery('.sq2').css('background-position-x'));
+                        jQuery('.sq2').css('background', 'none');
+                    } else {
+                        caliber = 1;
+                        prot_count += 1;
+                        jQuery('.sq2').css('background', 'url('+faces_img+') no-repeat');
+                        cur_faces = parseInt(jQuery('.sq4').css('background-position-x'))-400;
+                        jQuery('.sq2').css('background-position-x', cur_faces+'px');
+                        jQuery('.sq4').css('background', 'none');
+                    };
+                    tickSound.play();
+                    count_animation = 1;
+                    d12Val = 0;
+                } else {
+                    clearInterval(phaseOne);
+                    onEnd();
+                }
+            }
+        }, 250);
+    };
     jQuery('body').on('click', '.prot-start', function(event) {
         if(supportsStorage && localStorage.getItem('circle_protocol')){
             circle_protocol = localStorage.getItem('circle_protocol');
