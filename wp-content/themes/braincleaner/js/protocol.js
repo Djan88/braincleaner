a04492033df5a543cf464daee3ba4f01606e593a
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-10-15 10:44:48 +0300

    main fix-161

--- wp-content/themes/braincleaner/js/protocol.js
+++ wp-content/themes/braincleaner/js/protocol.js
@@ -39,7 +39,7 @@ jQuery(document).ready(function() {
             function(isConfirm){
                 if (isConfirm) {
                     var protocol = undefined;    
-                    jQuery(location).attr('href','/defra');
+                    jQuery(location).attr('href','/novyj-protokol');
                 }
             });
             var endSound = new buzz.sound( "/sounds/fin", {
