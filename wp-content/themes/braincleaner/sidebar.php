abea3e1d3e0661ee1dde01eb1ec5b8feff7d5cbb
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-08 11:42:56 +0300

    home fix-6

--- wp-content/themes/braincleaner/sidebar.php
+++ wp-content/themes/braincleaner/sidebar.php
@@ -1,14 +1,14 @@
 <div class="col-md-4 sidebar-zone">
-  <div class="list-group">
-    <div class="list-group-item active">
-      <h4 class="list-group-item-heading">Новости</h4>
+    <div class="list-group">
+        <div class="list-group-item active">
+            <h4 class="sidebar-heading"><a href="http://wizardmachine.ru/">wizardmachine.ru</a></h4>
+        </div>
     </div>
-    <?php if (!dynamic_sidebar("arhive-widget-area") ) : ?>
-    <?php endif; ?>
-  </div>
-  <div class="list-group">
-    <div class="list-group-item active">
-      <h4 class="sidebar-heading"><a href="http://wizardmachine.ru/">wizardmachine.ru</a></h4>
+    <div class="list-group">
+        <div class="list-group-item active">
+            <h4 class="list-group-item-heading">Новости</h4>
+        </div>
+        <?php if (!dynamic_sidebar("arhive-widget-area") ) : ?>
+        <?php endif; ?>
     </div>
-  </div>
 </div>
