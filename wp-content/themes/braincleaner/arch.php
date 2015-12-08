d6add2d81f37bf4efb57c2a377cf56fde169f031
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-08 11:18:53 +0300

    home fix-3

--- wp-content/themes/braincleaner/arch.php
+++ wp-content/themes/braincleaner/arch.php
@@ -1,4 +1,5 @@
-<h3><?php single_cat_title(); ?> <?php if(is_category(5)) { ?> Обучение методике +7(495) 255-05-61 <a href="mailto:yuchikurov@gmail.com">yuchikurov@gmail.com</a><?php } ?></h3>
+<?php if(is_category(5)) { ?><h4>Обучение методике +7(495) 255-05-61 <a href="mailto:yuchikurov@gmail.com">yuchikurov@gmail.com</a></h4><?php } ?>
+<h3><?php single_cat_title(); ?></h3>
 <div class="col-md-12 panel panel-default cat_description">
   <!-- <span class="glyphicon glyphicon-arrow-down"></span><span class="glyphicon glyphicon-arrow-up"> -->
   </span><?php echo category_description( ); ?>
