d08ef3fdfaea3074daa9f99e7b510888bc656ac4
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-09 16:03:07 +0300

    style fix-2

--- wp-content/themes/braincleaner/659.php
+++ wp-content/themes/braincleaner/659.php
@@ -1,12 +1,14 @@
 <div class="col-md-6" style="border-right: 1px solid #ddd; padding-top: 15px; padding-bottom: 15px;">
 	<div class="emo_img emo_util">
 		<h5>Утилизатор</h5>
+		<div class="emo_pelena"></div>
 		<img class="emo_lovushka" src="<?php bloginfo('template_url'); ?>/img/lovushka.png" alt="lovushka">
 	</div>
 </div>
 <div class="col-md-6" style="padding-top: 15px; padding-bottom: 15px;">
 	<div class="emo_img emo_usil">
 		<h5>Усилитель</h5>
+		<div class="emo_pelena"></div>
 		<img src="<?php bloginfo('template_url'); ?>/img/usilitel.png" alt="usilitel">
 	</div>
 </div>
