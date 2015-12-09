8939c73455848927efdc2b2d5ff63d7daa7bf540
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-09 15:13:47 +0300

    style fix-19

--- wp-content/themes/braincleaner/659.php
+++ wp-content/themes/braincleaner/659.php
@@ -1,10 +1,10 @@
 <div class="col-md-6" style="border-right: 1px solid #ddd">
-	<div class="659_img 659_util">
+	<div class="emo_img emo_util">
 		<img src="<?php bloginfo('template_url'); ?>/img/lovushka.png" alt="lovushka">
 	</div>
 </div>
 <div class="col-md-6">
-	<div class="659_img 659_usil">
+	<div class="emo_img emo_usil">
 		<img src="<?php bloginfo('template_url'); ?>/img/usilitel.png" alt="usilitel">
 	</div>
 </div>
