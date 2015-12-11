99042c359d2c65192a294856b4474c634c2f0b89
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-11 17:45:05 +0300

    emo fix 41

--- wp-content/themes/braincleaner/sidebar.php
+++ wp-content/themes/braincleaner/sidebar.php
@@ -3,7 +3,7 @@
         <div class="list-group-item active">
             <h4 class="list-group-item-heading">Новости</h4>
         </div>
-        <div class="list-group-item list-group-item-success">
+        <div class="list-group-item list-group-item-info">
             <h4 class="sidebar-heading list-group-item-heading">Обучение методике</h4>
             <p>+7(495) 255-05-61</p>
             <p><a target="_blank" href="mailto:yuchikurov@gmail.com">yuchikurov@gmail.com</a></p>
