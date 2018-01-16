9e3684169d2f6ea4b1b9f04a73d77376f018f885
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-01-16 18:11:18 +0300

    manual fix-4

--- wp-content/themes/braincleaner/index.php
+++ wp-content/themes/braincleaner/index.php
@@ -138,12 +138,15 @@
                             <div class="btn-group">
                               <a href="/category/defragmentaciya/" class="btn btn-default btn-procedure" data-toggle="tooltip" title="К списку протоколов"><span class="glyphicon glyphicon-arrow-left"></span> Назад</a>
                             </div>
-                            <div class="btn-group test">
-                              <button type="button" class="btn btn-default btn-status btn-auto active btn-procedure" data-status="auto" data-toggle="tooltip" title="Провести процедуру в автоматическом режиме">Авто режим</button>
-                            </div>
+                            <?php $only_manual = get_field('only_manual'); ?>
+                            <?php if($only_manual != true) { ?>
+                              <div class="btn-group">
+                                <button type="button" class="btn btn-default btn-status btn-auto active btn-procedure" data-status="auto" data-toggle="tooltip" title="Провести процедуру в автоматическом режиме">Авто режим</button>
+                              </div>
+                            <?php } ?>
                             <div class="btn-group">
                             <?php if(is_user_logged_in()) { ?> 
-                              <button type="button" class="btn btn-default btn-status btn-manual btn-procedure" data-status="manual" data-toggle="tooltip" title="Провести процедуру в режиме ручного управления">Ручное управление</button>
+                              <button type="button" class="btn btn-default btn-status btn-manual btn-procedure<?php if($only_manual == true) { ?> active <?php } ?>" data-status="manual" data-toggle="tooltip" title="Провести процедуру в режиме ручного управления">Ручное управление</button>
                             <?php } else { ?>
                               <button type="button" class="btn btn-default disabled btn-status btn-manual btn-procedure" data-status="manual" data-toggle="tooltip" title="Доступно только сертифицированным пользователям">Ручное управление</button>
                             <?php } ?>
