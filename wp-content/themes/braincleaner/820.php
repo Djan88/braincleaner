e06fbb1018c9495f9bdf0fef440d4f031973cc7f
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-06-15 17:36:44 +0300

    history-30

--- wp-content/themes/braincleaner/820.php
+++ wp-content/themes/braincleaner/820.php
@@ -1,31 +1,31 @@
 <?php if(is_user_logged_in()){ ?>
     <div class="row master_cards_wrapper shadow">
-        <div class="col-md-2 marakata_sim-wrap">
+        <div class="col-md-2 col-xs-2 marakata_sim-wrap">
             <div class="marakata_sim-inner">
                 <div class="marakata_sim marakata_sim_prot marakata_sim_from marakata_sim-1"></div>
             </div>
         </div>
-        <div class="col-md-2 marakata_sim-wrap">
+        <div class="col-md-2 col-xs-2 marakata_sim-wrap">
             <div class="marakata_sim-inner">
                 <div class="marakata_sim marakata_sim_prot marakata_sim_from marakata_sim-2"></div>
             </div>
         </div>
-        <div class="col-md-2 marakata_sim-wrap">
+        <div class="col-md-2 col-xs-2 marakata_sim-wrap">
             <div class="marakata_sim-inner">
                 <div class="marakata_sim marakata_sim_prot marakata_sim_from marakata_sim-3"></div>
             </div>
         </div>
-        <div class="col-md-2 marakata_sim-wrap">
+        <div class="col-md-2 col-xs-2 marakata_sim-wrap">
             <div class="marakata_sim-inner">
                 <div class="marakata_sim marakata_sim_prot marakata_sim_from marakata_sim-4"></div>
             </div>
         </div>
-        <div class="col-md-2 marakata_sim-wrap">
+        <div class="col-md-2 col-xs-2 marakata_sim-wrap">
             <div class="marakata_sim-inner">
                 <div class="marakata_dot"></div>
             </div>
         </div>
-        <div class="col-md-2 marakata_sim-wrap">
+        <div class="col-md-2 col-xs-2 marakata_sim-wrap">
             <div class="marakata_sim-inner">
                 <div class="marakata_sim marakata_sim_prot marakata_sim_from  marakata_sim-5"></div>
             </div>
@@ -40,7 +40,7 @@
         <div class="col"></div>
     </div>
     <div class="row">
-        <div class="history col-md-8 col-md-offset-2">
+        <div class="history col-md-8 col-md-offset-2 col-xs-12">
           <!-- <button type="button" class="close history_close">
             <span aria-hidden="true">&times;</span>
           </button> -->
