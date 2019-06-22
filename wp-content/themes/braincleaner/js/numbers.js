fe4070687745b0402b14ffce67ccbee3cac51b15
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-06-22 13:09:05 +0300

    brain-invers-21

--- wp-content/themes/braincleaner/js/numbers.js
+++ wp-content/themes/braincleaner/js/numbers.js
@@ -335,25 +335,6 @@ cur_date = function(){
     jQuery('.history_item_modal_date').text(cur_history_item.date)
   });
 
-  // save as img
-  jQuery('.save_img').on('click', function(event) {
-    jQuery('.for_print').css('display', 'block');
-    html2canvas(document.querySelector("#history_item_modal_content")).then(canvas => {
-      document.body.appendChild(canvas)
-      jQuery('.for_print').css('display', 'none');
-      jQuery(canvas).attr('id', 'history_canvas');
-      var c = document.getElementById("history_canvas");
-      var d = c.toDataURL("image/png");
-      var temp_elem = "<img src='"+d+"' alt='from canvas'/>";
-      jQuery('.saved_img').attr('href', d);
-      jQuery('.saved_img')[0].click();
-    });
-  });
-  jQuery('.history_wrapper').on('click', '.history_item_open', function(event) {
-    jQuery('.saved_img').attr('href', '')
-    jQuery('#history_canvas').detach();
-  });
-
 // INVERS
   jQuery('.invers_img').on('click', function(event) {
     jQuery(this).addClass('invers_img_rotated');
@@ -380,6 +361,25 @@ cur_date = function(){
   jQuery('.btn_refresh').on('click', function(event) {
     jQuery('.invers_img').removeClass('invers_img_rotated');
     jQuery(this).addClass('hidden');
+  });
+
+  // save as img
+  jQuery('.save_img').on('click', function(event) {
+    jQuery('.for_print').css('display', 'block');
+    html2canvas(document.querySelector("#history_item_modal_content")).then(canvas => {
+      document.body.appendChild(canvas)
+      jQuery('.for_print').css('display', 'none');
+      jQuery(canvas).attr('id', 'history_canvas');
+      var c = document.getElementById("history_canvas");
+      var d = c.toDataURL("image/png");
+      var temp_elem = "<img src='"+d+"' alt='from canvas'/>";
+      jQuery('.saved_img').attr('href', d);
+      jQuery('.saved_img')[0].click();
+    });
+  });
+  jQuery('.history_wrapper').on('click', '.history_item_open', function(event) {
+    jQuery('.saved_img').attr('href', '')
+    jQuery('#history_canvas').detach();
   });
 
 });
