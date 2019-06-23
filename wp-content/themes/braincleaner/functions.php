a045c4c2a5d2441eee53e18a2004641475f71efd
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-06-23 14:32:31 +0300

    brain-invers-32

--- wp-content/themes/braincleaner/functions.php
+++ wp-content/themes/braincleaner/functions.php
@@ -2625,6 +2625,9 @@ add_filter('login_redirect', 'users_redirect');
 
                                     // create a new image from file 
                                     $vImg = @imagecreatefromjpeg($sTempFileName);
+                                    if(current_user_can('administrator')) {
+                                      print_r ($vImg);
+                                    }
                                     break;
                                 case IMAGETYPE_PNG:
                                     $sExt = '.png';
