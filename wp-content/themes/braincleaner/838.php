f7dd66c810973725b24a68ac55c26a36faa45a36
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-12-20 13:36:57 +0300

    vspread-4

--- wp-content/themes/braincleaner/838.php
+++ wp-content/themes/braincleaner/838.php
@@ -3,7 +3,27 @@
         <div class="col-xs-12">
           <div class="vspread">
             <img src="<?php bloginfo('template_url'); ?>/img/template_.jpg" alt="template">
-            <div class="vspread_zone vspread_zone_v-">V-</div>
+            <div class="vspread_zone vspread_zone_V0">V0</div>
+            <div class="vspread_zone vspread_zone_V1">V1</div>
+            <div class="vspread_zone vspread_zone_V2">V2</div>
+            <div class="vspread_zone vspread_zone_V3">V3</div>
+            <div class="vspread_zone vspread_zone_V4">V4</div>
+            <div class="vspread_zone vspread_zone_V5">V5</div>
+            <div class="vspread_zone vspread_zone_V-">V-</div>
+
+            <div class="vspread_zone vspread_zone_D2_up">D2</div>
+            <div class="vspread_zone vspread_zone_D2">D2</div>
+            <div class="vspread_zone vspread_zone_D3">D3</div>
+            <div class="vspread_zone vspread_zone_D4">D4</div>
+            <div class="vspread_zone vspread_zone_D5">D5</div>
+            <div class="vspread_zone vspread_zone_D6">D6</div>
+
+            <div class="vspread_zone vspread_zone_S2_up">S2</div>
+            <div class="vspread_zone vspread_zone_S2">S2</div>
+            <div class="vspread_zone vspread_zone_S3">S3</div>
+            <div class="vspread_zone vspread_zone_S4">S4</div>
+            <div class="vspread_zone vspread_zone_S5">S5</div>
+            <div class="vspread_zone vspread_zone_S6">S6</div>
           </div>
         </div>
     </div>
