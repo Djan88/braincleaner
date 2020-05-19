e7b4015b4e43ee7c7cd23a073f69243b0ed32e6a
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-19 13:32:31 +0300

    login fix-4

--- wp-content/themes/braincleaner/home.php
+++ wp-content/themes/braincleaner/home.php
@@ -20,25 +20,56 @@
               <h3 class="panel-title">Вход на сайт</h3>
             </div>
             <div class="panel-body">
-              <?php 
-                $args = array(
-                  'echo' => true,
-                  'redirect' => site_url( $_SERVER['REQUEST_URI'] ), 
-                  'form_id' => 'loginform',
-                  'label_username' => 'Логин',
-                  'label_password' => 'Пароль',
-                  'label_remember' => 'Запомнить меня',
-                  'label_log_in' => 'Войти',
-                  'id_username' => 'user_login',
-                  'id_password' => 'user_pass',
-                  'id_remember' => 'rememberme',
-                  'id_submit' => 'wp-submit',
-                  'remember' => true,
-                  'value_username' => NULL,
-                  'value_remember' => false
-                );
-              ?>
-              <?php wp_login_form( $args );?>
+              <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post" class="form-horizontal user-in" role="form">
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Логин</label>
+                  <div class="input-group input-group-lg">
+                    <span class="input-group-addon">@</span>
+                    <input type="text" name="log" id="user_login" class="input form-control" value="<?php echo esc_attr($user_login); ?>" size="20" />
+                  </div>
+                </div>
+                <?php
+                /**
+                 * Fires following the 'Password' field in the login form.
+                 *
+                 * @since 2.1.0
+                 */
+                do_action( 'login_form' );
+                ?>
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Пароль</label>
+                  <div class="input-group input-group-lg">
+                    <span class="input-group-addon">*</span>
+                    <input type="password" name="pwd" id="user_pass" class="input form-control" value="" size="20" />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-sm-offset-2 col-sm-10">
+                    <div class="checkbox">
+                      <label>
+                        <label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-sm-offset-2 col-sm-10">
+                    <!-- <button type="submit" class="btn btn-default">Войти</button> -->
+                    <!-- <a href="inside.html" class="btn btn-default">Войти</a> -->
+                            <input type="submit" name="wp-submit" id="wp-submit" class="btn btn-primary" value="<?php esc_attr_e('Log In'); ?>" />
+                    <?php   if ( $interim_login ) { ?>
+                            <input type="hidden" name="interim-login" value="1" />
+                    <?php   } else { ?>
+                            <input type="hidden" name="redirect_to" value="/" />
+                    <?php   } ?>
+                    <?php   if ( $customize_login ) : ?>
+                            <input type="hidden" name="customize-login" value="1" />
+                    <?php   endif; ?>
+                            <input type="hidden" name="testcookie" value="1" />
+                            <a class="btn btn-default" href="/category/defragmentaciya/" style="margin-left:10px;">Бесплатный доступ</a>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
