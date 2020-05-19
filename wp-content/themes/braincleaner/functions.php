7f036d69b6691a9a2afa00016b2bcc0daab05ac6
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-19 13:56:27 +0300

    login fix-5

--- wp-content/themes/braincleaner/functions.php
+++ wp-content/themes/braincleaner/functions.php
@@ -16,6 +16,20 @@ if (function_exists('add_theme_support')) {
  add_theme_support('menus');
 }
 
+//fix for cookie error while login.
+setcookie(TEST_COOKIE, 'WP Cookie check', 0, COOKIEPATH, COOKIE_DOMAIN); 
+if ( SITECOOKIEPATH != COOKIEPATH ) 
+setcookie(TEST_COOKIE, 'WP Cookie check', 0, SITECOOKIEPATH, COOKIE_DOMAIN);
+
+function is_user_role( $role, $user_id = null ) {
+  $user = is_numeric( $user_id ) ? get_userdata( $user_id ) : wp_get_current_user();
+
+  if( ! $user )
+    return false;
+
+  return in_array( $role, (array) $user->roles );
+}
+
 // function users_redirect(){
 // wp_redirect(site_url('/'));
 // die();
