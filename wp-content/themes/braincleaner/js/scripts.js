be85039cc77457a9826d8c1a45ac70028d9eb2f7
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-12-25 11:09:10 +0300

    regress-19

--- wp-content/themes/braincleaner/js/scripts.js
+++ wp-content/themes/braincleaner/js/scripts.js
@@ -457,19 +457,19 @@ jQuery(document).ready(function() {
           console.log(handle_val);
           jQuery('.reverce_clean_graph').removeAttr('style')
           if (handle_val <= 55) {
-            jQuery('.regress-result')text('2');
+            jQuery('.regress-result').text('2');
           } else if (handle_val > 55 && handle_val <= 110) {
-            jQuery('.regress-result')text('3');
+            jQuery('.regress-result').text('3');
           } else if (handle_val > 110 && handle_val <= 165) {
-            jQuery('.regress-result')text('4');
+            jQuery('.regress-result').text('4');
           }  else if (handle_val > 165 && handle_val <= 225) {
-            jQuery('.regress-result')text('5');
+            jQuery('.regress-result').text('5');
           }  else if (handle_val > 225 && handle_val <= 275) {
-            jQuery('.regress-result')text('6');
+            jQuery('.regress-result').text('6');
           } else if (handle_val > 275 && handle_val <= 340) {
-            jQuery('.regress-result')text('7');
+            jQuery('.regress-result').text('7');
           } else if (handle_val > 340) {
-            jQuery('.regress-result')text('8');
+            jQuery('.regress-result').text('8');
           }
         }
       });
