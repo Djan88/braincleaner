fd80d815fefdb3a2e6df944ac8f5b652bce65912
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-12-25 10:20:37 +0300

    regress-11

--- wp-content/themes/braincleaner/js/scripts.js
+++ wp-content/themes/braincleaner/js/scripts.js
@@ -432,7 +432,7 @@ jQuery(document).ready(function() {
       var handle_val;
       jQuery("#slider").slider({
         min: 0,
-        max: 120,
+        max: 200,
         create: function() {
           handle.text(jQuery(this).slider("value"));
         },
